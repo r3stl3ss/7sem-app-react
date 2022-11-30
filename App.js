85453bfc9aee7f0a2ebcaac20f24af9ffcc6495e
r3stl3ss <55531045+r3stl3ss@users.noreply.github.com>
2022-11-30 22:18:18 +0300

    second screen

--- App.js
+++ App.js
@@ -7,187 +7,328 @@ import {
   Dimensions,
   ScrollView,
 } from 'react-native';
-import logo from './assets/picture.png';
+
 
 export default function App() {
   return (
     <ScrollView style={{ backgroundColor: 'grey' }}>
       <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147131.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>John Doe</Text>
+          <Text>30 sep 11:14pm</Text>
           </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp1Wq8GZJCNEniFbHKD5YCsxm3Mfwmxnh1zQ&usqp=CAU'
+          }}
+          ></Image>
         </View>
       </View>
       <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147131.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>John Doe</Text>
+          <Text>31 sep 11:15pm</Text>
           </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyHtT01NkPRmYezUuwD2nQIdMvzWrVLHpdhg&usqp=CAU'
+          }}
+          ></Image>
         </View>
       </View>
       <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147131.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>John Doe</Text>
+          <Text>1 oct 11:16pm</Text>
           </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp1Wq8GZJCNEniFbHKD5YCsxm3Mfwmxnh1zQ&usqp=CAU'
+          }}
+          ></Image>
         </View>
       </View>
       <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147131.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>John Doe</Text>
+          <Text>2 oct 11:17pm</Text>
           </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyHtT01NkPRmYezUuwD2nQIdMvzWrVLHpdhg&usqp=CAU'
+          }}
+          ></Image>
         </View>
       </View>
       <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147131.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>John Doe</Text>
+          <Text>3 oct 11:18pm</Text>
           </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp1Wq8GZJCNEniFbHKD5YCsxm3Mfwmxnh1zQ&usqp=CAU'
+          }}
+          ></Image>
         </View>
       </View>
       <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147131.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>Johnny Guitar</Text>
+          <Text>4 oct 11:19pm</Text>
           </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyHtT01NkPRmYezUuwD2nQIdMvzWrVLHpdhg&usqp=CAU'
+          }}
+          ></Image>
         </View>
       </View>
       <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147131.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>John Doe</Text>
+          <Text>5 oct 11:20pm</Text>
           </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp1Wq8GZJCNEniFbHKD5YCsxm3Mfwmxnh1zQ&usqp=CAU'
+          }}
+          ></Image>
         </View>
       </View>
       <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147134.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>Johnny Doe</Text>
+          <Text>8 oct 11:23pm</Text>
           </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyHtT01NkPRmYezUuwD2nQIdMvzWrVLHpdhg&usqp=CAU'
+          }}
+          ></Image>
         </View>
       </View>
       <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147131.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>John Doe</Text>
+          <Text>9 oct 11:24pm</Text>
           </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp1Wq8GZJCNEniFbHKD5YCsxm3Mfwmxnh1zQ&usqp=CAU'
+          }}
+          ></Image>
+        </View>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147133.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>Mark Doe</Text>
+          <Text>10 oct 11:25pm</Text>
+          </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyHtT01NkPRmYezUuwD2nQIdMvzWrVLHpdhg&usqp=CAU'
+          }}
+          ></Image>
+        </View>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147132.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>Helen Doe</Text>
+          <Text>11 oct 11:26pm</Text>
+          </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp1Wq8GZJCNEniFbHKD5YCsxm3Mfwmxnh1zQ&usqp=CAU'
+          }}
+          ></Image>
+        </View>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147136.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>Adam Smith</Text>
+          <Text>12 oct 11:27pm</Text>
+          </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyHtT01NkPRmYezUuwD2nQIdMvzWrVLHpdhg&usqp=CAU'
+          }}
+          ></Image>
+        </View>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.item}>
+          <View style={styles.data}>
+            <Image 
+            style={styles.image}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/147/147138.png'
+            }}></Image>
+          
+          <View>
+          <Text style={{fontSize:15, fontWeight: 'bold'}}>Frank Nelson</Text>
+          <Text>13 oct 11:28pm</Text>
+          </View>
+          </View>
+          <Image
+          style={styles.call}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp1Wq8GZJCNEniFbHKD5YCsxm3Mfwmxnh1zQ&usqp=CAU'
+          }}
+          ></Image>
         </View>
       </View>
     </ScrollView>
   );
 }
 
-const screen = Dimensions.get('screen');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 2,
-  },
-  card: {
     backgroundColor: '#fff',
-    width: screen.width * 1,
   },
-  header: {
-    flexDirection: 'row',
+  item: {
+    flex:1,
+    alignItems: "center",
+    flexDirection: "row",
+    borderBottomWidth: 0.5,
+    borderBottomColor: 'black',
     justifyContent: 'space-between',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
   },
-  followText: {
-    fontWeight: 'bold',
-    color: '#0095f6',
-    width: 240,
+  data: {
+    alignItems: "center",
+    flexDirection: "row",
   },
-  AvatarImage: {
-    height: 75,
-    width: 75,
-    resizeMode: 'stretch',
-    margin: 15,
+  image: {
+    width:50,
+    height: 50,
+    borderRadius: 30,
+    margin: 15
   },
+  call: {
+    width:30,
+    height: 30,
+    borderRadius: 30,
+    marginRight: 15,
+  }
 });
