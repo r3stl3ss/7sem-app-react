fcc99db1aba70553f469b52a0e83c5853980eec4
r3stl3ss <55531045+r3stl3ss@users.noreply.github.com>
2022-12-02 16:26:40 +0300

    fifth screen with screenshot

--- App.js
+++ App.js
@@ -1,193 +1,45 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  Dimensions,
-  ScrollView,
-} from 'react-native';
-import logo from './assets/picture.png';
+import { StyleSheet,Image, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
   return (
-    <ScrollView style={{ backgroundColor: 'grey' }}>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
-          </View>
-        </View>
+    <View style={styles.body}>
+        <Image
+          style={{width:150, height: 150, borderRadius:75, margin: 30}}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMKNsBt5QHBOwzvpCQXQ-bc2Aq8O3825_RbQ&usqp=CAU'
+          }}
+        />
+        <Text style={{color: '#6c6c6c', fontSize: 18, fontWeight: 'bold'}}>Congratulation, your order is accepted</Text>
+        <Text style={{color: '#a7a7a7', width: '70%', marginTop: 20, fontWeight: 'bold'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua.</Text>  
+        <TouchableOpacity style={styles.button}>
+          <Text style={{color:'#fff'}}>Continue</Text>
+        </TouchableOpacity>
+      
+    
+        
+        
       </View>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <View style={styles.card}>
-          <View style={styles.header}>
-            <Image
-              style={styles.AvatarImage}
-              source={{
-                uri: 'https://image.winudf.com/v2/image/Y29tLmR1eHQucHJvZi5mcm9udF9pY29uXzBfNzM5NThjNGY/icon.png?w=340&fakeurl=1',
-              }}></Image>
-            <Text style={styles.followText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam
-            </Text>
-          </View>
-        </View>
-      </View>
-    </ScrollView>
+    
   );
 }
 
-const screen = Dimensions.get('screen');
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 2,
-  },
-  card: {
     backgroundColor: '#fff',
-    width: screen.width * 1,
+    width: "100%",
+    marginTop: 4 ,
+    alignItems: 'center',
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-  },
-  followText: {
-    fontWeight: 'bold',
-    color: '#0095f6',
-    width: 240,
-  },
-  AvatarImage: {
-    height: 75,
-    width: 75,
-    resizeMode: 'stretch',
-    margin: 15,
-  },
+  button: {
+    marginTop: 5,
+    backgroundColor: '#0080ff',
+    height: 40,
+    width: '25%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20
+  }
 });
