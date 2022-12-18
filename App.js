bd7cc1b0ccc31289f691797b4ee6e58a5355918e
r3stl3ss <55531045+r3stl3ss@users.noreply.github.com>
2022-12-18 16:03:01 +0300

    example  of mobx

--- App.js
+++ App.js
@@ -1,41 +1,37 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import Constants from 'expo-constants';
-import { useState } from 'react';
+import React from 'react'
+import { View, Button, Text } from 'react-native'
+import { makeAutoObservable } from 'mobx'
+import { observer } from 'mobx-react-lite'
 
-export default function App() {
-  const [counter, setCounter] = useState(0);
-  return (
-    <>
-      <View style={styles.container}>
-        <View>
-          <Button 
-            title="Decrease"
-            onPress={() => setCounter(counter - 1)} 
-          />
-        </View>
-        <Text>{counter}</Text>
-        <Button
-        onPress={() => setCounter(counter + 1)}
-          title="Increase"
-        />
-      </View>
-    </>
-  );
+class Counter {
+  count = 0
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  increment() {
+    this.count += 1
+  }
+
+  decrement() {
+    this.count -= 1
+  }
+
+  reset() {
+    this.count = 0
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#7CA1B4",
-    flex: 1,
-    alignItems: "center", // ignore this - we'll come back to it
-    justifyContent: "space-around", // ignore this - we'll come back to it
-    flexDirection: "row",
-  },
-  square: {
-    backgroundColor: "#7cb48f",
-    width: 100,
-    height: 100,
-    margin: 4,
-  },
-});
+const counter = new Counter()
+
+const App = observer(() => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>{counter.count}</Text>
+    <Button onPress={() => counter.increment()} title="+" />
+    <Button onPress={() => counter.decrement()} title="-" />
+    <Button onPress={() => counter.reset()} title="Reset" />
+  </View>
+))
+
+export default App
