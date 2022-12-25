07ec1eb423aa1cf0f86ff1819cc43679ba08b473
r3stl3ss <55531045+r3stl3ss@users.noreply.github.com>
2022-12-25 15:55:08 +0300

    mobx clicker

--- App.js
+++ App.js
@@ -1,41 +1,32 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import Constants from 'expo-constants';
-import { useState } from 'react';
+import React from 'react'
+import { View, Button, Text } from 'react-native'
+import { makeAutoObservable } from 'mobx'
+import { observer } from 'mobx-react-lite'
 
-export default function App() {
-  const [counter, setCounter] = useState(0);
-  return (
-    <>
-      <View style={styles.container}>
-        <View>
-          <Button 
-            title="Decrease"
-            onPress={() => setCounter(counter - 1)} 
-          />
-        </View>
-        <Text>{counter}</Text>
-        <Button
-        onPress={() => setCounter(counter + 1)}
-          title="Increase"
-        />
-      </View>
-    </>
-  );
+class Clicker {
+    clicks = 0
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    addClick() {
+        this.clicks += 1
+    }
+
+    deleteClick() {
+      this.clicks -= 1
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#7CA1B4",
-    flex: 1,
-    alignItems: "center", // ignore this - we'll come back to it
-    justifyContent: "space-around", // ignore this - we'll come back to it
-    flexDirection: "row",
-  },
-  square: {
-    backgroundColor: "#7cb48f",
-    width: 100,
-    height: 100,
-    margin: 4,
-  },
-});
+const clicker = new Clicker()
+
+const App = observer(() => (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ fontSize: clicker.clicks}}>{clicker.clicks}</Text>
+        <Button onPress={() => clicker.addClick()} title="increase" />
+        <Button onPress={() => clicker.deleteClick()} title="decrease" />
+    </View>
+))
+
+export default App;
